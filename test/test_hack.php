d419ede4d1db3d26c9651f5cc1340819c66a673f
akalend <akalend@mail.ru>
2016-02-25 23:20:40 +0300

    merge test from master

--- test/test_hack.php
+++ test/test_hack.php
@@ -5,16 +5,9 @@
 *
 */
 
-<<<<<<< HEAD
-
 $v = Vector{'one', 2, true };
 $m = Map{"one" => 1, "two" => 2};
 $arr = [ 'Pupkin',  null,  42 ,  [ 'sss' => 12, 0 => '2333'  ], [7,13, 21, 33,'*****'] ];
-=======
-$v = Vector{1,2,3};
-$s = Set{'xxx','yyy','zzz'};
-$arr = [ 'Pupkin',  (array)$v,  (array)$s , [1,5,['x'=>'****']], [ 'sss' => 12, 0 => '2333'  ]];
->>>>>>> develope
 
 if (1) {
 	$data = $arr;
@@ -22,17 +15,9 @@ if (1) {
 	$data = msgpack_pack( $data );
 
 	// echo $data , PHP_EOL;
-<<<<<<< HEAD
-	file_put_contents('/tmp/data.bin', $data );
-=======
 	// file_put_contents('data.bin', $data );
->>>>>>> develope
 }
 
 // $data = file_get_contents('../data.bin');
 $data = msgpack_unpack( $data);
-<<<<<<< HEAD
-print_r($data);
-=======
 var_dump($data); 
->>>>>>> develope
